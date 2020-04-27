f0183c6b12e09b21b2cb6033d24bd703f835ab86
susumoa <tobiasbori@gmail.com>
2020-04-27 10:42:11 +0100

    feat get API key, create dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
+const aylien = require('aylien_textapi');
+
+const textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY,
+});
 
 const app = express();
 
